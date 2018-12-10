8fefe5fe93f5065df21af77235af3872ed1c10c2
juerg <juerg.maier@pobox.ch>
2018-12-10 16:22:33 +0100

    watchDog with clients

--- rpcSend.py
+++ rpcSend.py
@@ -6,10 +6,13 @@ import rpcReceive
 def publishLog(msg):
 
     for i, c in enumerate(rpcReceive.clientList):
-        #print(f"log to {xmlrpcClientList[i]}")
-        try:
-            c['conn'].root.exposed_log(msg)
 
-        except Exception as e:
-            print(f"exception in publishLog with c.callerName: {str(e)}")
+        if c['replyConn'] is not None:
+
+            try:
+                c['replyConn'].root.exposed_log(msg)
+
+            except Exception as e:
+                print(f"exception in publishLog with {c['clientId']}: {e}")
+                c['replyConn'] = None
 
